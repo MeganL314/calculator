45b4df9f424c9da55cda9e33599b0be7447c14d7
Megan Lynch <megantherese314@gmail.com>
2025-07-21 15:08:33 -0400

    Finish calculator

--- javascript.js
+++ javascript.js
@@ -38,55 +38,111 @@ const operate = function(operator, a, b){
     }
 }
 
+
 const screen = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
+
 let equation = [];
 let screen_text = "";
+let justEvaluated = false;
 
-// Fill in new
+
+
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
+
         // run operate() if '=' sign is hit
         if(button.textContent == '='){
-            let expression = buildExpression(equation);
-            console.log(expression);
-            let answer = operate(expression[1], expression[0], expression[2]);
-            console.log(answer);
-            screen_text = answer;
-            screen.textContent = screen_text;
-            equation = [answer];
+
+            // check to make sure an operator was included in equation
+            if (!['+','-','/','x'].some(val => equation.includes(val)) ||
+        equation.length<3){
+                screen.textContent = "No";
+                console.log("NO, Wrong");
+            }
+
+            // if there was an operator, check to make sure there isn't division by zero
+            else{
+                let expression = buildExpression(equation);
+                console.log(expression);
+                if(expression[1] == '/' && expression[2] == 0){
+                    screen.textContent = "No, can't.";
+                }
+
+                // else run operate() if '=' sign is hit
+                // re-assign screen content and begin equation with answer
+                else{
+                    justEvaluated = true;
+                    let answer = operate(expression[1], expression[0], expression[2]);
+                    console.log(answer);
+                    screen_text = answer;
+                    screen.textContent = screen_text;
+                    equation = [answer];
+                }
+            }
         }
+
         // clear the screen if clear is hit
         else if(button.textContent == 'clear'){
             equation = [];
             screen_text = "";
             screen.textContent = screen_text;
         }
+
         // save the equation but clear the screen if +-/x is hit
         else if(button.textContent == '+' || button.textContent == '-' || button.textContent == 'x' ||
             button.textContent == '/'
         ){
             // when any of the equation operators are press, check if they're already there
+            // if so, replace with the most recent equation operator that was pressed
             if(['+','-','/','x'].some(val => equation.includes(val))){
-                operate(equation);
-                // screen_text should have the result of operate
-                screen_text = "";
-                screen.textContent = screen_text;
-                equation = [];
+                const last = equation[equation.length-1];
+
+                if(['+','-','/','x'].includes(last)){
+                    equation = equation.slice(0, equation.length-1);
+                    equation.push(button.textContent);
+                    screen_text = screen_text.slice(0, screen_text.length-1) + button.textContent;
+                    screen.textContent = screen_text;
+                }
+                
+                // if not, run operate() and display an answer
+                else{
+                    justEvaluated = true;
+                    let expression = buildExpression(equation);
+                    let answer = operate(expression[1], expression[0], expression[2]);
+                    screen_text = answer + button.textContent;
+                    screen.textContent = screen_text;
+                    equation = [answer, button.textContent];
+                    console.log(equation)
+                }
             }
+            // if an equation operator was pressed but there isn't one yet, continue building equation
             else{
                 equation.push(button.textContent);
                 screen_text = screen_text + button.textContent;
                 screen.textContent = screen_text;                
             }
         }
-        // else just save the equation
+        // else just continue building the equation
+
+
+
         else{
-            // build equation first
-            equation.push(button.textContent);
-            screen_text = screen_text + button.textContent;
-            screen.textContent = screen_text;
+            // build equation if justEvaluated = FALSE;
+
+            if (justEvaluated != true){
+                equation.push(button.textContent);
+                screen_text = screen_text + button.textContent;
+                screen.textContent = screen_text;
+            }
+            else if (justEvaluated = true){
+                justEvaluated = false;
+                equation = [button.textContent];
+                screen_text = button.textContent;
+                screen.textContent = screen_text;
+            }
+
         }
     //console.log(equation);
     });
